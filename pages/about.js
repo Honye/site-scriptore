9f362fd8ec466b6ef5e2ec5496280a18e1ae8355
Honye <hongye.jun@qq.com>
2023-01-30 15:13:23 +0800

    feat: 优化关于页请求

--- pages/about.js
+++ pages/about.js
@@ -65,10 +65,6 @@ const About = (props) => {
             variant="h6"
           >{ user?.login || '未登录' }</Typography>
         </Stack>
-        <AvatarGroup max={6}>
-          <Avatar sx={{ width: '66px', height: '66px' }} alt='Jun' src='https://w.wallhaven.cc/full/jx/wallhaven-jx811m.png' />
-          <Avatar sx={{ width: '66px', height: '66px' }} alt='Jackie' src='https://w.wallhaven.cc/full/9d/wallhaven-9d6wg8.jpg' />
-        </AvatarGroup>
         <Typography variant='body1' gutterBottom>Scriptore 是一个 Scriptable 脚本仓库</Typography>
         <Typography variant='body1' gutterBottom>使用浏览器访问时需要安装 <Link href='https://raw.githubusercontent.com/Honye/scriptable-scripts/master/dist/Installer.js'>Installer</Link> 脚本才能正常使用</Typography>
         <Typography variant='body1' gutterBottom>在 Scriptable 应用内使用可安装 <Link href='https://raw.githubusercontent.com/Honye/scriptable-scripts/master/dist/Scriptore.js'>Scriptore</Link> 脚本</Typography>
@@ -136,16 +132,16 @@ const About = (props) => {
 
 /** @type {import('next').GetServerSideProps} */
 export const getServerSideProps = async ({ req, query }) => {
-  const { token } = req.cookies;
+  const props = {};
 
-  const user = await fetchUser({ token })
+  const { token } = req.cookies;
+  if (token) {
+    const user = await fetchUser({ token })
       .then((resp) => resp.ok ? resp.json() : null);
-  
-  return {
-    props: {
-      user
-    }
-  };
+    props.user = user;
+  }
+
+  return { props };
 };
 
 export default About;
