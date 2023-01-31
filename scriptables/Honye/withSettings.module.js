be28fe22f98e0f27bbe52ddeabb8b6b956105ec0
iRobot <irobot@imarkr.com>
2023-01-31 14:47:06 +0800

    Commit by Robot

---
+++ scriptables/Honye/withSettings.module.js
@@ -0,0 +1,659 @@
+/**
+ * 轻松实现桌面组件可视化配置
+ *
+ * - 颜色选择器及更多表单控件
+ * - 快速预览
+ *
+ * GitHub: https://github.com/honye
+ *
+ * @version 1.2.2
+ * @author Honye
+ */
+
+const { i18n, presentSheet, useCache, useFileManager } = importModule('utils.module')
+
+/**
+ * @returns {Promise<Settings>}
+ */
+const readSettings = async () => {
+  const localFM = useFileManager()
+  let settings = localFM.readJSON('settings.json')
+  if (settings) {
+    console.log('[info] use local settings')
+    return settings
+  }
+
+  const iCloudFM = useFileManager({ useICloud: true })
+  settings = iCloudFM.readJSON('settings.json')
+  if (settings) {
+    console.log('[info] use iCloud settings')
+  }
+  return settings
+}
+
+/**
+ * @param {Record<string, unknown>} data
+ * @param {{ useICloud: boolean; }} options
+ */
+const writeSettings = async (data, { useICloud }) => {
+  const fm = useFileManager({ useICloud })
+  fm.writeJSON('settings.json', data)
+}
+
+const removeSettings = async (settings) => {
+  const cache = useFileManager({ useICloud: settings.useICloud })
+  FileManager.local().remove(
+    FileManager.local().joinPath(
+      cache.cacheDirectory,
+      'settings.json'
+    )
+  )
+}
+
+const moveSettings = (useICloud, data) => {
+  const localFM = useFileManager()
+  const iCloudFM = useFileManager({ useICloud: true })
+  const [i, l] = [
+    FileManager.local().joinPath(
+      iCloudFM.cacheDirectory,
+      'settings.json'
+    ),
+    FileManager.local().joinPath(
+      localFM.cacheDirectory,
+      'settings.json'
+    )
+  ]
+  try {
+    writeSettings(data, { useICloud })
+    const fm = FileManager.local()
+    if (useICloud) {
+      if (fm.fileExists(l)) fm.remove(l)
+    } else {
+      if (fm.fileExists(i)) fm.remove(i)
+    }
+  } catch (e) {
+    console.error(e)
+  }
+}
+
+/**
+ * @typedef {object} NormalFormItem
+ * @property {string} name
+ * @property {string} label
+ * @property {'text'|'number'|'color'|'select'|'date'|'cell'} [type]
+ *  - HTML <input> type 属性
+ *  - `'cell'`: 可点击的
+ * @property {{ label: string; value: unknown }[]} [options]
+ * @property {unknown} [default]
+ */
+/**
+ * @typedef {Pick<NormalFormItem, 'label'|'name'> & { type: 'group', items: FormItem[] }} GroupFormItem
+ */
+/**
+ * @typedef {Omit<NormalFormItem, 'type'> & { type: 'page' } & Pick<Options, 'formItems'|'onItemClick'>} PageFormItem 单独的页面
+ */
+/**
+ * @typedef {NormalFormItem|GroupFormItem|PageFormItem} FormItem
+ */
+/**
+ * @typedef {Record<string, unknown>} Settings
+ * @property {boolean} useICloud
+ * @property {string} [backgroundImage]
+ */
+/**
+ * @typedef {object} Options
+ * @property {(data: {
+ *  settings: Settings;
+ *  family?: typeof config.widgetFamily;
+ * }) => ListWidget | Promise<ListWidget>} render
+ * @property {string} [head] 顶部插入 HTML
+ * @property {FormItem[]} [formItems]
+ * @property {(item: FormItem) => void} [onItemClick]
+ * @property {string} [homePage] 右上角分享菜单地址
+ * @property {(data: any) => void} [onWebEvent]
+ */
+/**
+ * @template T
+ * @typedef {T extends infer O ? {[K in keyof O]: O[K]} : never} Expand
+ */
+
+const previewsHTML =
+`<div class="actions">
+  <button class="preview" data-size="small"><i class="iconfont icon-yingyongzhongxin"></i>${i18n(['Small', '预览小号'])}</button>
+  <button class="preview" data-size="medium"><i class="iconfont icon-daliebiao"></i>${i18n(['Medium', '预览中号'])}</button>
+  <button class="preview" data-size="large"><i class="iconfont icon-dantupailie"></i>${i18n(['Large', '预览大号'])}</button>
+</div>`
+
+const copyrightHTML =
+`<footer>
+  <div class="copyright">© UI powered by <a href="javascript:invoke('safari','https://www.imarkr.com');">iMarkr</a>.</div>
+</footer>`
+
+/**
+ * @param {Expand<Options>} options
+ * @param {boolean} [isFirstPage]
+ * @returns {Promise<ListWidget|undefined>} 在 Widget 中运行时返回 ListWidget，其它无返回
+ */
+const present = async (options, isFirstPage) => {
+  const {
+    formItems = [],
+    onItemClick,
+    render,
+    head,
+    homePage = 'https://www.imarkr.com',
+    onWebEvent
+  } = options
+  const cache = useCache()
+
+  let settings = await readSettings() || {}
+  const imgPath = FileManager.local().joinPath(
+    cache.cacheDirectory,
+    'bg.png'
+  )
+
+  if (config.runsInWidget) {
+    const widget = await render({ settings })
+    if (settings.backgroundImage) {
+      widget.backgroundImage = FileManager.local().readImage(imgPath)
+    }
+    Script.setWidget(widget)
+    return widget
+  }
+
+  // ====== web start =======
+  const style =
+`:root {
+  --color-primary: #007aff;
+  --divider-color: rgba(60,60,67,0.36);
+  --card-background: #fff;
+  --card-radius: 10px;
+  --list-header-color: rgba(60,60,67,0.6);
+}
+* {
+  -webkit-user-select: none;
+  user-select: none;
+}
+body {
+  margin: 10px 0;
+  -webkit-font-smoothing: antialiased;
+  font-family: "SF Pro Display","SF Pro Icons","Helvetica Neue","Helvetica","Arial",sans-serif;
+  accent-color: var(--color-primary);
+}
+input {
+  -webkit-user-select: auto;
+  user-select: auto;
+}
+body {
+  background: #f2f2f7;
+}
+button {
+  font-size: 16px;
+  background: var(--color-primary);
+  color: #fff;
+  border-radius: 8px;
+  border: none;
+  padding: 0.24em 0.5em;
+}
+button .iconfont {
+  margin-right: 6px;
+}
+.list {
+  margin: 15px;
+}
+.list__header {
+  margin: 0 20px;
+  color: var(--list-header-color);
+  font-size: 13px;
+}
+.list__body {
+  margin-top: 10px;
+  background: var(--card-background);
+  border-radius: var(--card-radius);
+  border-radius: 12px;
+  overflow: hidden;
+}
+.form-item {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  font-size: 16px;
+  min-height: 2em;
+  padding: 0.5em 20px;
+  position: relative;
+}
+.form-item--link .icon-arrow_right {
+  color: #86868b;
+}
+.form-item + .form-item::before {
+  content: "";
+  position: absolute;
+  top: 0;
+  left: 20px;
+  right: 0;
+  border-top: 0.5px solid var(--divider-color);
+}
+.form-item .iconfont {
+  margin-right: 4px;
+}
+.form-item input,
+.form-item select {
+  font-size: 14px;
+  text-align: right;
+}
+.form-item input[type="checkbox"] {
+  width: 1.25em;
+  height: 1.25em;
+}
+input[type="number"] {
+  width: 4em;
+}
+input[type="date"] {
+  min-width: 6.4em;
+}
+input[type='checkbox'][role='switch'] {
+  position: relative;
+  display: inline-block;
+  appearance: none;
+  width: 40px;
+  height: 24px;
+  border-radius: 24px;
+  background: #ccc;
+  transition: 0.3s ease-in-out;
+}
+input[type='checkbox'][role='switch']::before {
+  content: '';
+  position: absolute;
+  left: 2px;
+  top: 2px;
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  background: #fff;
+  transition: 0.3s ease-in-out;
+}
+input[type='checkbox'][role='switch']:checked {
+  background: var(--color-primary);
+}
+input[type='checkbox'][role='switch']:checked::before {
+  transform: translateX(16px);
+}
+.actions {
+  margin: 15px;
+}
+.copyright {
+  margin: 15px;
+  margin-inline: 18px;
+  font-size: 12px;
+  color: #86868b;
+}
+.copyright a {
+  color: #515154;
+  text-decoration: none;
+}
+.preview.loading {
+  pointer-events: none;
+}
+.icon-loading {
+  display: inline-block;
+  animation: 1s linear infinite spin;
+}
+@keyframes spin {
+  0% {
+    transform: rotate(0);
+  }
+  100% {
+    transform: rotate(1turn);
+  }
+}
+@media (prefers-color-scheme: dark) {
+  :root {
+    --divider-color: rgba(84,84,88,0.65);
+    --card-background: #1c1c1e;
+    --list-header-color: rgba(235,235,245,0.6);
+  }
+  body {
+    background: #000;
+    color: #fff;
+  }
+}
+`
+
+  const js =
+`(() => {
+  const settings = ${JSON.stringify(settings)}
+  const formItems = ${JSON.stringify(formItems)}
+
+  window.invoke = (code, data) => {
+    window.dispatchEvent(
+      new CustomEvent(
+        'JBridge',
+        { detail: { code, data } }
+      )
+    )
+  }
+
+  const formData = {};
+
+  const createFormItem = (item) => {
+    const value = settings[item.name] ?? item.default ?? null
+    formData[item.name] = value;
+    const label = document.createElement("label");
+    label.className = "form-item";
+    const div = document.createElement("div");
+    div.innerText = item.label;
+    label.appendChild(div);
+    if (item.type === 'select') {
+      const select = document.createElement('select')
+      select.className = 'form-item__input'
+      select.name = item.name
+      select.value = value
+      for (const opt of (item.options || [])) {
+        const option = document.createElement('option')
+        option.value = opt.value
+        option.innerText = opt.label
+        option.selected = value === opt.value
+        select.appendChild(option)
+      }
+      select.addEventListener('change', (e) => {
+        formData[item.name] = e.target.value
+        invoke('changeSettings', formData)
+      })
+      label.appendChild(select)
+    } else if (
+      item.type === 'cell' ||
+      item.type === 'page'
+    ) {
+      label.classList.add('form-item--link')
+      const icon = document.createElement('i')
+      icon.className = 'iconfont icon-arrow_right'
+      label.appendChild(icon)
+      label.addEventListener('click', () => {
+        const { name } = item
+        switch (name) {
+          case 'backgroundImage':
+            invoke('chooseBgImg')
+            break
+          case 'reset':
+            reset()
+            break
+          default:
+            invoke('itemClick', item)
+        }
+      })
+    } else {
+      const input = document.createElement("input")
+      input.className = 'form-item__input'
+      input.name = item.name
+      input.type = item.type || "text";
+      input.enterKeyHint = 'done'
+      input.value = value
+      // Switch
+      if (item.type === 'switch') {
+        input.type = 'checkbox'
+        input.role = 'switch'
+        input.checked = value
+        if (item.name === 'useICloud') {
+          input.addEventListener('change', (e) => {
+            invoke('moveSettings', e.target.checked)
+          })
+        }
+      }
+      if (item.type === 'number') {
+        input.inputMode = 'decimal'
+      }
+      if (input.type === 'text') {
+        input.size = 12
+      }
+      input.addEventListener("change", (e) => {
+        formData[item.name] =
+          item.type === 'switch'
+          ? e.target.checked
+          : item.type === 'number'
+          ? Number(e.target.value)
+          : e.target.value;
+        invoke('changeSettings', formData)
+      });
+      label.appendChild(input);
+    }
+    return label
+  }
+
+  const createList = (list, title) => {
+    const fragment = document.createDocumentFragment()
+
+    let elBody;
+    for (const item of list) {
+      if (item.type === 'group') {
+        const grouped = createList(item.items, item.label)
+        fragment.appendChild(grouped)
+      } else {
+        if (!elBody) {
+          const groupDiv = fragment.appendChild(document.createElement('div'))
+          groupDiv.className = 'list'
+          if (title) {
+            const elTitle = groupDiv.appendChild(document.createElement('div'))
+            elTitle.className = 'list__header'
+            elTitle.textContent = title
+          }
+          elBody = groupDiv.appendChild(document.createElement('div'))
+          elBody.className = 'list__body'
+        }
+        const label = createFormItem(item)
+        elBody.appendChild(label)
+      }
+    }
+    return fragment
+  }
+
+  const fragment = createList(formItems)
+  document.getElementById('settings').appendChild(fragment)
+
+  for (const btn of document.querySelectorAll('.preview')) {
+    btn.addEventListener('click', (e) => {
+      const target = e.currentTarget
+      target.classList.add('loading')
+      const icon = e.currentTarget.querySelector('.iconfont')
+      const className = icon.className
+      icon.className = 'iconfont icon-loading'
+      const listener = (event) => {
+        const { code } = event.detail
+        if (code === 'previewStart') {
+          target.classList.remove('loading')
+          icon.className = className
+          window.removeEventListener('JWeb', listener);
+        }
+      }
+      window.addEventListener('JWeb', listener)
+      invoke('preview', e.currentTarget.dataset.size)
+    })
+  }
+
+  const setFieldValue = (name, value) => {
+    const input = document.querySelector(\`.form-item__input[name="\${name}"]\`)
+    if (!input) return
+    if (input.type === 'checkbox') {
+      input.checked = value
+    } else {
+      input.value = value
+    }
+  }
+
+  const reset = (items = formItems) => {
+    for (const item of items) {
+      if (item.type === 'group') {
+        reset(item.items)
+      } else if (item.type === 'page') {
+        continue;
+      } else {
+        setFieldValue(item.name, item.default)
+      }
+    }
+    invoke('removeSettings', formData)
+  }
+})()`
+
+  const html =
+`<html>
+  <head>
+    <meta name='viewport' content='width=device-width, user-scalable=no'>
+    <link rel="stylesheet" href="//at.alicdn.com/t/c/font_3772663_kmo790s3yfq.css" type="text/css">
+    <style>${style}</style>
+  </head>
+  <body>
+  ${head || ''}
+  <section id="settings"></section>
+  ${isFirstPage ? (previewsHTML + copyrightHTML) : ''}
+  <script>${js}</script>
+  </body>
+</html>`
+
+  const webView = new WebView()
+  await webView.loadHTML(html, homePage)
+
+  const clearBgImg = () => {
+    delete settings.backgroundImage
+    const fm = FileManager.local()
+    if (fm.fileExists(imgPath)) {
+      fm.remove(imgPath)
+    }
+  }
+
+  const chooseBgImg = async () => {
+    const { option } = await presentSheet({
+      options: [
+        { key: 'choose', title: i18n(['Choose photo', '选择图片']) },
+        { key: 'clear', title: i18n(['Clear background image', '清除背景图']) }
+      ],
+      cancelText: i18n(['Cancel', '取消'])
+    })
+    switch (option?.key) {
+      case 'choose': {
+        try {
+          const image = await Photos.fromLibrary()
+          cache.writeImage('bg.png', image)
+          settings.backgroundImage = imgPath
+          writeSettings(settings, { useICloud: settings.useICloud })
+        } catch (e) {}
+        break
+      }
+      case 'clear':
+        clearBgImg()
+        writeSettings(settings, { useICloud: settings.useICloud })
+        break
+    }
+  }
+
+  const injectListener = async () => {
+    const event = await webView.evaluateJavaScript(
+      `(() => {
+        const controller = new AbortController()
+        const listener = (e) => {
+          completion(e.detail)
+          controller.abort()
+        }
+        window.addEventListener(
+          'JBridge',
+          listener,
+          { signal: controller.signal }
+        )
+      })()`,
+      true
+    ).catch((err) => {
+      console.error(err)
+      throw err
+    })
+    const { code, data } = event
+    switch (code) {
+      case 'preview': {
+        const widget = await render({ settings, family: data })
+        const { backgroundImage } = settings
+        if (backgroundImage) {
+          widget.backgroundImage = FileManager.local().readImage(backgroundImage)
+        }
+        webView.evaluateJavaScript(
+          'window.dispatchEvent(new CustomEvent(\'JWeb\', { detail: { code: \'previewStart\' } }))',
+          false
+        )
+        widget[`present${data.replace(data[0], data[0].toUpperCase())}`]()
+        break
+      }
+      case 'safari':
+        Safari.openInApp(data, true)
+        break
+      case 'changeSettings':
+        settings = { ...settings, ...data }
+        writeSettings(settings, { useICloud: settings.useICloud })
+        break
+      case 'moveSettings':
+        settings.useICloud = data
+        moveSettings(data, settings)
+        break
+      case 'removeSettings':
+        settings = { ...settings, ...data }
+        clearBgImg()
+        removeSettings(settings)
+        break
+      case 'chooseBgImg':
+        await chooseBgImg()
+        break
+      case 'itemClick':
+        if (data.type === 'page') {
+          await present(data)
+        } else {
+          await onItemClick?.(data, { settings })
+        }
+        break
+      case 'native':
+        onWebEvent?.(data)
+        break
+    }
+    injectListener()
+  }
+
+  injectListener()
+  webView.present()
+  // ======= web end =========
+}
+
+/**
+ * @param {Options} options
+ */
+const withSettings = async (options) => {
+  const { formItems, onItemClick, ...restOptions } = options
+  return present({
+    formItems: [
+      {
+        label: i18n(['Common', '通用']),
+        type: 'group',
+        items: [
+          {
+            label: i18n(['Sync with iCloud', 'iCloud 同步']),
+            type: 'switch',
+            name: 'useICloud',
+            default: false
+          },
+          {
+            label: i18n(['Background image', '背景图']),
+            type: 'cell',
+            name: 'backgroundImage'
+          },
+          {
+            label: i18n(['Reset', '重置']),
+            type: 'cell',
+            name: 'reset'
+          }
+        ]
+      },
+      {
+        label: i18n(['Settings', '设置']),
+        type: 'group',
+        items: formItems
+      }
+    ],
+    ...restOptions
+  }, true)
+}
+
+module.exports = {
+  withSettings
+}
