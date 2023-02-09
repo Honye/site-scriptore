aeca4aafe64318ec11e70e392e3d683e91fdc5a0
Honye <hongye.jun@qq.com>
2023-02-09 18:38:09 +0800

    scriptable: Astrology v1.2.0
    - 增加大号组件显示未来星运
    - 使用缓存

--- data/scripts.js
+++ data/scripts.js
@@ -172,15 +172,16 @@ export const widgets = [
   },
   {
     name: 'Astrology',
-    version: '1.1.0',
+    version: '1.2.0',
     icon: 'hub',
     intro: '星座运势',
     content: '边框颜色默认显示运势幸运色，用户可以自定义\n\n修改边框颜色后可以通过重置恢复为幸运色\n\n',
     files: [
-      'https://cdn.jsdelivr.net/gh/honye/scriptable-scripts@8e5067e45cb10fcf27873769e2572bc79c0998f9/dist/Astrology.js'
+      'https://cdn.jsdelivr.net/gh/honye/scriptable-scripts@62b11ada210466304174bba8bbc7f4f0b80d505c/dist/Astrology.js'
     ],
     snapshots: [
-      'https://cdn.jsdelivr.net/gh/Honye/scriptable-scripts@8e5067e45cb10fcf27873769e2572bc79c0998f9/docs/assets/Astrology_1.png'
+      'https://cdn.jsdelivr.net/gh/Honye/scriptable-scripts@8e5067e45cb10fcf27873769e2572bc79c0998f9/docs/assets/Astrology_1.png',
+      'https://cdn.jsdelivr.net/gh/Honye/scriptable-scripts@master/docs/assets/Astrology_2.png'
     ]
   },
   {
