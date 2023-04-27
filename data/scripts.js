5813d7d30a7a4072a427ef6a04e95079fd39c825
Honye <hongye.jun@qq.com>
2023-04-27 08:42:26 +0800

    scriptale: OpenAI v1.0.0
    增加 Session 过期通知

--- data/scripts.js
+++ data/scripts.js
@@ -217,14 +217,14 @@ export const widgets = [
   },
   {
     name: 'OpenAI',
-    version: '0.2.0',
+    version: '1.0.0',
     icon: 'ac_unit',
     intro: 'OpenAI 账户使用情况',
     content: '\n\nSession Key 需自行从网页 https://platform.openai.com/account/usage 抓取' +
       '\n\n抓取请求 https://api.openai.com/dashboard/billing/credit_grants 的请求头中的 Authorization' +
       '\n\n不要前面的 Bearer',
     files: [
-      'https://cdn.jsdelivr.net/gh/honye/scriptable-scripts@eaef09f541909665cc618003b6b07c8f4037c405/dist/OpenAI.js'
+      'https://cdn.jsdelivr.net/gh/honye/scriptable-scripts@ff0746cfeceaa5d7c9c88357dd1fa0883a0b0743/dist/OpenAI.js'
     ]
   }
 ];
