142035f28aa12c55c2d2f4ffef8bf1db48e27b53
Honye <hongye.jun@qq.com>
2023-09-21 08:09:29 +0800

    scriptable: Calendar v1.4.1
    修复提醒时间多余显示 00:00

--- data/scripts.js
+++ data/scripts.js
@@ -60,11 +60,11 @@ export const widgets = [
   },
   {
     name: 'Calendar',
-    version: '1.4.0',
+    version: '1.4.1',
     icon: 'calendar_month',
     intro: '和 Apple 日历一样美观的日历，农历显示，可作为打卡日历',
     files: [
-      'https://cdn.jsdelivr.net/gh/Honye/scriptable-scripts@855c34588c6f28439fe148cc92923253ff59607c/dist/Calendar.js',
+      'https://cdn.jsdelivr.net/gh/Honye/scriptable-scripts@8b142d4f0af59078afbf52abf4750fd62080b360/dist/Calendar.js',
     ],
     snapshots: [
       'https://cdn.jsdelivr.net/gh/Honye/scriptable-scripts@master/docs/assets/calendar.jpeg',
