bbf98a582deed4de3eb0e3b637e13c4f5d138807
Honye <hongye.jun@qq.com>
2023-10-28 23:51:06 +0800

    scriptable: 湖北联通 v1.3.1
    - 配置导入/导出单独一页显示
    - 修复下拉组件过长

--- data/scripts.js
+++ data/scripts.js
@@ -137,11 +137,11 @@ export const widgets = [
   },
   {
     name: '湖北联通',
-    version: '1.3.0',
+    version: '1.3.1',
     icon: 'phone_iphone',
     intro: '湖北联通余额信息',
     files: [
-      'https://cdn.jsdelivr.net/gh/Honye/scriptable-scripts@c56f59130e33a42d98626bb7a832e81d502a8de5/dist/%E6%B9%96%E5%8C%97%E8%81%94%E9%80%9A.js'
+      'https://cdn.jsdelivr.net/gh/Honye/scriptable-scripts@804061d95509860a8d6727b75692289cbf165355/dist/%E6%B9%96%E5%8C%97%E8%81%94%E9%80%9A.js'
     ],
     snapshots: [
       'https://cdn.jsdelivr.net/gh/Honye/scriptable-scripts@master/docs/assets/10010_small_light.jpg',
