716cde19f96def97d1dec817c037dab949bec039
Honye <hongye.jun@qq.com>
2023-11-19 22:24:59 +0800

    feat: SEO

--- pages/_document.js
+++ pages/_document.js
@@ -11,7 +11,8 @@ class MyDocument extends Document {
       <Html lang="zh-CN" data-color-mode="auto" data-light-theme="light" data-dark-theme="dark">
         <Head>
           <meta name="application-name" content="Scriptore" />
-          <meta name="description" content="Scriptable scripts store" />
+          <meta name="description" content="Scriptore 是一个开源的 Scriptable 脚本和小组件商店" />
+          <meta name="keywords" content="Scriptore,Scriptable,Scripts,Widgets,小组件" />
           <meta name='apple-mobile-web-app-title' content="Scriptore" />
           <meta name="apple-mobile-web-app-capable" content="yes" />
           <link rel='apple-touch-icon' sizes='144x144' href='/icons/apple-touch-icon-144x144.png' />
