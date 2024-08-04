cac2320cde369e55866f5970b5a2b4dbee5486ce
Honye <hongye.jun@qq.com>
2024-08-04 18:08:28 +0800

    scriptable: Olympics v1.1.1
    更好的屏幕兼容性

--- data/scripts.js
+++ data/scripts.js
@@ -258,11 +258,11 @@ export const widgets = [
   },
   {
     name: 'Olympics',
-    version: '1.1.0',
+    version: '1.1.1',
     intro: '2024 巴黎奥运会奖牌榜',
     content: '2024 巴黎奥运会奖牌榜',
     files: [
-      'https://cdn.jsdelivr.net/gh/honye/scriptable-scripts@f51437dd200ebd9a096245ada02d9a2e3880fd3b/dist/Olympics.js'
+      'https://cdn.jsdelivr.net/gh/honye/scriptable-scripts@faef8648c484cbf2dcb05e6bf54c3e89d01bdb01/dist/Olympics.js'
     ]
   }
 ];
