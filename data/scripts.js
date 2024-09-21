0f14c4497967817540717b914c15def3b24ac4db
Honye <hongye.jun@qq.com>
2024-09-21 19:54:29 +0800

    scriptable(module): utils v1.1.1
    增加 iPhone 16 尺寸

--- data/scripts.js
+++ data/scripts.js
@@ -292,11 +292,11 @@ export const modules = [
   },
   {
     name: 'utils',
-    version: '1.1.0',
+    version: '1.1.1',
     icon: 'handyman',
     intro: '工具集',
     files: [
-      'https://cdn.jsdelivr.net/gh/Honye/scriptable-scripts@4b78bd7178c7c0e559bc88fcece3a558168f16f9/src/utils.module.js',
+      'https://cdn.jsdelivr.net/gh/Honye/scriptable-scripts@6224a5529f506a6c84f2fd720ecbbf33e6d87d90/src/utils.module.js',
     ],
   },
   {
